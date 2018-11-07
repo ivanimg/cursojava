edfe35e6d56ac270ba2b792d9827e7eb57b77bbd
ivanimg <ivanmorenogarciaimg@gmail.com>
2018-11-07 16:53:41 +0100

    prueba despues de error

--- Practica1/src/es/indra/practica1/Consola.java
+++ Practica1/src/es/indra/practica1/Consola.java
@@ -2,17 +2,22 @@ package es.indra.practica1;
 
 import java.io.IOException;
 
+import org.xml.sax.InputSource;
+
 public class Consola {
 
 	public static void main(String[] args) {
+		String nombre;
+
 		
-		System.out.println("Introduce tu nombre: ");
 		try {
-			System.in.read();
+			nombre = System.in.read(b, off, len);
+			System.in.
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Hola "+);
 	}
 
 }

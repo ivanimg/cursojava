69e52f6e4b8b24cf6255e334b5ca6c0ad01fcd0e
ivanimg <ivanmorenogarciaimg@gmail.com>
2018-11-20 17:38:03 +0100

    martes 20

--- spring-mvc-demo-basico/src/main/java/net/impactotecnologico/mvc/demo/controller/CategoriaController.java
+++ spring-mvc-demo-basico/src/main/java/net/impactotecnologico/mvc/demo/controller/CategoriaController.java
@@ -1,5 +1,21 @@
 package net.impactotecnologico.mvc.demo.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import net.impactotecnologico.mvc.demo.model.Categoria;
+
 public class CategoriaController {
 
+	@RequestMapping(path = { "/categoria" }, method = RequestMethod.GET)
+	public String sayHello(Model model) {
+
+		Categoria c = new Categoria(1, "Nombre ejemplo");
+
+		model.addAttribute("nombre", c.getNombre());
+		model.addAttribute("categoria", c);
+
+		return "categoria";
+	}
 }
